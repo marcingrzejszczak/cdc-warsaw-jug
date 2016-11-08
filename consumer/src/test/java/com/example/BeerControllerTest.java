334f5df5e15b00b90d1b47be3526bcfb62fe1995
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-11-08 12:18:51 +0100

    Initial commit

---
+++ consumer/src/test/java/com/example/BeerControllerTest.java
@@ -0,0 +1,56 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+// @AutoConfigureStubRunner
+public class BeerControllerTest extends AbstractTest {
+
+	@Autowired  MockMvc mockMvc;
+
+	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
+
+	}
+
+	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
+
+	}
+}
+
+
+/*
+
+
+
+	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(asJson(new Person("marcin", 22))))
+				.andExpect(status().isOk())
+				.andExpect(content().string("THERE YOU GO"));
+	}
+
+	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(asJson(new Person("marcin", 17))))
+				.andExpect(status().isOk())
+				.andExpect(content().string("GET LOST"));
+	}
+
+ */
+
