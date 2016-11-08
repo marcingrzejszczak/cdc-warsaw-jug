3daa49e9c4dcaab266b6bf77ed69d41bc40fa0f4
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-11-08 12:29:57 +0100

    Added missing impl

--- consumer/src/test/java/com/example/BeerControllerTest.java
+++ consumer/src/test/java/com/example/BeerControllerTest.java
@@ -7,27 +7,41 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Marcin Grzejszczak
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
-// @AutoConfigureStubRunner
+@AutoConfigureStubRunner(workOffline = true, ids = "com.example:beer-api-producer:+:stubs:8090")
 public class BeerControllerTest extends AbstractTest {
 
 	@Autowired  MockMvc mockMvc;
 
 	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
-
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 22)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("THERE YOU GO"));
 	}
 
 	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
-
+		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 17)).getJson()))
+				.andExpect(status().isOk())
+				.andExpect(content().string("GET LOST"));
 	}
 }
 
@@ -38,16 +52,16 @@ public class BeerControllerTest extends AbstractTest {
 
 	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(asJson(new Person("marcin", 22))))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 22)).getJson()))
 				.andExpect(status().isOk())
 				.andExpect(content().string("THERE YOU GO"));
 	}
 
 	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(asJson(new Person("marcin", 17))))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.write(new Person("marcin", 17)).getJson()))
 				.andExpect(status().isOk())
 				.andExpect(content().string("GET LOST"));
 	}
